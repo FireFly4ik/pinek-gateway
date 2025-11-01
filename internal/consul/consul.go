e617a570ca303ec6bd27d434dd7dba36b79c2317
FireFly4ik <andreybatman06@gmail.com>
2025-11-01 17:16:28 +0300

    fix: checkIds and ids of service with multi-instances

--- internal/consul/consul.go
+++ internal/consul/consul.go
@@ -14,6 +14,7 @@ type ConsulProvider struct {
 	client  *api.Client
 	name    string
 	checkId string
+	id      string
 }
 
 func NewProvider(envConf *config.Config) *ConsulProvider {
@@ -27,7 +28,8 @@ func NewProvider(envConf *config.Config) *ConsulProvider {
 		address: envConf.Consul.Address,
 		client:  client,
 		name:    envConf.Consul.Name,
-		checkId: envConf.Consul.CheckId,
+		checkId: envConf.Consul.CheckId + "(" + envConf.Port + ")",
+		id:      envConf.Consul.Name + "(" + envConf.Port + ")",
 	}
 
 	err = cp.registerService(envConf)
@@ -46,7 +48,7 @@ func (p *ConsulProvider) registerService(envConf *config.Config) error {
 		DeregisterCriticalServiceAfter: envConf.Consul.DeregisterTTL,
 		TTL:                            envConf.Consul.RegisterTTL,
 		TLSSkipVerify:                  true,
-		CheckID:                        envConf.Consul.CheckId,
+		CheckID:                        envConf.Consul.CheckId + "(" + envConf.Port + ")",
 	}
 
 	port, _ := strconv.Atoi(envConf.Port)
@@ -54,7 +56,7 @@ func (p *ConsulProvider) registerService(envConf *config.Config) error {
 	register := &api.AgentServiceRegistration{
 		Address: envConf.Address,
 		Port:    port,
-		ID:      envConf.Consul.Name,
+		ID:      envConf.Consul.Name + "(" + envConf.Port + ")",
 		Name:    envConf.Consul.Name,
 		Tags:    []string{"gateway"},
 		Check:   check,
@@ -87,7 +89,7 @@ func (p *ConsulProvider) updateHealthCheck(envConf *config.Config) {
 }
 
 func (p *ConsulProvider) DeregisterService() {
-	err := p.client.Agent().ServiceDeregister(p.name)
+	err := p.client.Agent().ServiceDeregister(p.id)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to deregister service from Consul")
 	}
