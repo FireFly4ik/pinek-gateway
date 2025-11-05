515ad93b946adf0b63102c5bc05abc0577b01af3
FireFly4ik <andreybatman06@gmail.com>
2025-11-05 22:43:15 +0300

    fix: try to reregister to consul

--- internal/consul/consul.go
+++ internal/consul/consul.go
@@ -78,12 +78,22 @@ func (p *ConsulProvider) updateHealthCheck(envConf *config.Config) {
 	}
 
 	ticker := time.NewTicker(ttl)
+	errors := 0
 
 	for {
 		err = p.client.Agent().UpdateTTL(p.checkId, "online", api.HealthPassing)
 		if err != nil {
+			errors++
+			if errors == 10 {
+				err := p.registerService(envConf)
+				if err != nil {
+					panic("Consul is not working or no internet connection")
+				}
+				errors = 0
+			}
 			log.Error().Err(err).Msg("Failed to update Consul health check")
 		}
+		errors = 0
 		<-ticker.C
 	}
 }
