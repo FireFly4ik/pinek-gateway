8569496c213da76033af267b57cb1be1c115b7cc
FireFly4ik <andreybatman06@gmail.com>
2025-11-06 15:49:09 +0300

    refactor: update swagger with new ip address

--- docs/docs.go
+++ docs/docs.go
@@ -621,7 +621,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "178.140.128.113:8080",
+	Host:             "82.202.138.76:8080",
 	BasePath:         "/api/v1/",
 	Schemes:          []string{},
 	Title:            "Pinek Gateway API",
