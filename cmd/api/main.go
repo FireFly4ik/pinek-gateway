85d32927d243b92573df60dff0b0468259bbc566
FireFly4ik <andreybatman06@gmail.com>
2025-11-06 15:49:45 +0300

    refactor: update swagger with new ip address

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,7 +27,7 @@ import (
 // @license.name MIT
 // @license.url https://opensource.org/licenses/MIT
 
-// @host 178.140.128.113:8080
+// @host 82.202.138.76:8080
 // @BasePath /api/v1/
 
 // @securityDefinitions.apikey ApiKeyAuth
